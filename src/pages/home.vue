bb640bb98bd6c2a439c6e62f7949b6ea4095cd43
Oksana Kulakova <oksana5566@yandex.ru>
2021-08-02 20:57:51 +0500

    вынесла stats в отдельный компонент

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -11,16 +11,7 @@
           {{ item.descr }}
         </template>
         <template v-slot:footer>
-          <div class="card-stats">
-            <div
-              v-for="(stat, index) in item.info"
-              :key="index"
-              class="one-third"
-            >
-              <div class="stat-value">{{ stat.value }}</div>
-              <div class="stat">{{ stat.title }}</div>
-            </div>
-          </div>
+          <CardStats :stats="item.info"></CardStats>
         </template>
       </Card>
     </slide>
@@ -36,6 +27,7 @@
 import items from '@/seeders/items.js'
 
 import Card from '@/components/UI/Card'
+import CardStats from '@/components/UI/CardStats'
 
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Navigation } from 'vue3-carousel'
@@ -43,6 +35,7 @@ import { Carousel, Slide, Navigation } from 'vue3-carousel'
 export default {
   components: {
     Card,
+    CardStats,
     Carousel,
     Slide,
     Navigation
